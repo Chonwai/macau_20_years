8295e618da9478129d6dea22a702d02969d235cc
Edison Un <chonwaiun@gmail.com>
2019-08-25 20:51:22 +0800

    Add the environment to production and set the public path

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,12 @@
 module.exports = {
-  pluginOptions: {
-    i18n: {
-      locale: 'cn',
-      fallbackLocale: 'cn',
-      localeDir: 'locales',
-      enableInSFC: false
-    }
-  }
+    pluginOptions: {
+        i18n: {
+            locale: 'cn',
+            fallbackLocale: 'cn',
+            localeDir: 'locales',
+            enableInSFC: false
+        }
+    },
+    publicPath: process.env.NODE_ENV === 'production' ?
+        '/macau20years/' : '/'
 }
